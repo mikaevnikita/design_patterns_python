6001bbbee0ca8951fa0f3a864477ca28413939c0
Andrew <vzbjik@gmail.com>
2018-04-04 20:38:57 +0300

    +Factory method +Abstract factory

---
+++ factory_method.py
@@ -0,0 +1,38 @@
+class GreekGetter(object):
+
+    """A simple localizer a la gettext"""
+
+    def __init__(self):
+        self.trans = dict(dog="σκύλος", cat="γάτα")
+
+    def get(self, msgid):
+        """We'll punt if we don't have a translation"""
+        return self.trans.get(msgid, str(msgid))
+
+
+class EnglishGetter(object):
+
+    """Simply echoes the msg ids"""
+
+    def get(self, msgid):
+        return str(msgid)
+
+
+def get_localizer(language="English"):
+    """The factory method"""
+    languages = dict(English=EnglishGetter, Greek=GreekGetter)
+    return languages[language]()
+
+
+if __name__ == '__main__':
+    # Create our localizers
+    e, g = get_localizer(language="English"), get_localizer(language="Greek")
+    # Localize some text
+    for msgid in "dog parrot cat bear".split():
+        print(e.get(msgid), g.get(msgid))
+
+### OUTPUT ###
+# dog σκύλος
+# parrot parrot
+# cat γάτα
+# bear bear
